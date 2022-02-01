94613d25ced020751bb264c9312d4e109b24b072
subhrajyoti <subhrajyotisankarsau@gmail.com>
2022-02-01 20:52:08 +0530

    feb1

---
+++ February/bestTimeTOBuyAndSellStock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n==0)return 0;
+        int min_price = prices[0];
+        int cost = 0;
+        int maxCost = 0;
+        for(int i=0;i<n;i++){
+            min_price = min(min_price,prices[i]);
+            cost = prices[i]-min_price;
+            maxCost = max(maxCost,cost);
+        }
+        return maxCost;
+    }
+};
