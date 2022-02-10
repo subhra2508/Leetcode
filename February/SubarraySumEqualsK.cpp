bdc7eeaf22f3fe22261e792ba1448353231694f7
subhrajyoti <subhrajyotisankarsau@gmail.com>
2022-02-10 11:43:30 +0530

    feb10

---
+++ February/SubarraySumEqualsK.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0;
+        unordered_map<int,int>mp;
+        int currSum = 0;
+        for(int i=0;i<nums.size();i++){
+            currSum +=nums[i];
+            if(currSum==k){
+                count++;
+            }
+            if(mp.find(currSum-k)!=mp.end()){
+                count+=mp[currSum-k];
+            }
+            mp[currSum]++;
+        }
+        return count;
+    }
+};
