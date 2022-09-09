38760a36472045e5d537822cb4ca0c875350ff42
subhrajyoti <subhrajyotisankarsau@gmail.com>
2022-09-09 23:46:17 +0530

    sept9

---
+++ sept/theNumberOfWeakCharacterInTheGame.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    static bool comp(vector<int>&a,vector<int>&b){
+        if(a[0]!=b[0]){
+            return a[0]>b[0];
+        }
+        return a[1]<b[1];
+    }
+    int numberOfWeakCharacters(vector<vector<int>>& properties) {
+        sort(properties.begin(),properties.end(),comp);
+        int maxTillNow = INT_MIN;
+        int count = 0;
+        for(auto p:properties){
+            if(maxTillNow > p[1]){
+                count++;
+            }
+            else 
+                maxTillNow = p[1];
+        }
+        return count;
+    }
+};
