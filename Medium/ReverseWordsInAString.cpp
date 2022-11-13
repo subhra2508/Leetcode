af7edf5b413afbec487421ead3f30a6bd31d15a6
subhrajyoti <subhrajyotisankarsau@gmail.com>
2022-11-13 12:27:32 +0530

    13thNov2022

---
+++ Medium/ReverseWordsInAString.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string>v;
+        stringstream str(s);
+        string word;
+        while(str>>word){
+            v.push_back(word);
+        }
+        reverse(v.begin(),v.end());
+        string ans="";
+        for(auto it:v){
+            ans = ans+" "+it;
+        }
+        ans.erase(0,1);//from index no-0 delete 1 char
+        return ans;
+    }
+};
