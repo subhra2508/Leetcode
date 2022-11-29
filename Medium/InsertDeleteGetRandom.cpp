0c176554f7fbad380835503626adae4ed18e9087
subhrajyoti <subhrajyotisankarsau@gmail.com>
2022-11-29 21:42:32 +0530

    29Nov2022

---
+++ Medium/InsertDeleteGetRandom.cpp
@@ -0,0 +1,42 @@
+class RandomizedSet {
+public:
+unordered_set<int>s;
+    RandomizedSet() {
+        bool insert(int val);
+        bool remove(int val);
+        int getRandom;
+    }
+    
+    bool insert(int val) {
+         if(s.find(val)==s.end()){
+             s.insert(val);
+             return true;
+         }
+         return false;
+    }
+    
+    bool remove(int val) {
+        if(s.find(val)!=s.end()){
+            s.erase(val);
+            return true;
+        }
+        return false;
+    }
+    
+    int getRandom() {
+        int idx = rand()%s.size();
+        auto itr = s.begin();
+        for(int i=0;i<idx;i++){
+            itr++;
+        }
+        return *itr;
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
